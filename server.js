61c2049ed52cb5c6a84f2cfbd6499eeedd40f8e3
Sandberg, Nathan <Nathan.Sandberg@cdk.com>
2021-02-26 14:19:05 -0600

    Changed the index.js file to server.js.

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const bodyParser = require('body-parser');
+
+const axios = require('axios');
+
+app.use(bodyParser.json({ extended: true }), express.json());
+
+const CLIENT_ID = "Your API Key";
+const CLIENT_SECRET = "Your API Secret";
+
+const token = Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64');
+
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build'));
+});
+
+app.post(`/token`, (req, res) => {
+    console.log(req.headers);
+    console.log(token);
+    console.log(req.headers.accept);
+    console.log(req.headers['content-type']);
+    const headers = {
+      accept: req.headers.accept,
+      authorization: 'Basic ' + token,
+    };
+    axios
+      .post(
+        `https://identity.fortellis.io/oauth2/aus1p1ixy7YL8cMq02p7/v1/token/?scope=anonymous&grant_type=client_credentials`,
+        null,
+        {
+          params: {},
+          headers: headers,
+        }
+      )
+      .then((response) => {
+        console.log(response.data);
+        res.send(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
+
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build'));
+});
